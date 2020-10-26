6208a55d831796b825ccb0f29fdab5bbf41c17c2
Colton O'Connor <colton.shawn.oconnor@gmail.com>
2020-10-26 20:56:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,7 +14,9 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   
-  new_hash = base_hash
+  new_hash = {
+    railroads: {}
+  }
   new_hash[:railroads] = {pieces => 4, rent_in_dollars => {} , names => {}} 
 
 end
